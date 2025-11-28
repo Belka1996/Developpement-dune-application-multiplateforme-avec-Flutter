ad4deda5137d62edc8ad9f2926c16094e2492e7a
Belka1996 <medjkounebelkacem2019@gmail.com>
2025-11-28 15:50:05 -0500

    Affichage du titre de la tâche dans TaskDetailPage

--- lib/pages/task_detail_page.dart
+++ lib/pages/task_detail_page.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import '../models/task.dart';
 
 class TaskDetailPage extends StatelessWidget {
   const TaskDetailPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Detaila de la Tache")),
+    final Task task = ModalRoute.of(context)!.settings.arguments as Task;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(task.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              task.title,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
     );
   }
 }
