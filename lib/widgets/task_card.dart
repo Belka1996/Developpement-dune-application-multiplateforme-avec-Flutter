8352a7859b21c41e2d00db2fe02180711cbc9325
Belka1996 <medjkounebelkacem2019@gmail.com>
2025-11-29 01:17:40 -0500

    Touche final

--- lib/widgets/task_card.dart
+++ lib/widgets/task_card.dart
@@ -3,45 +3,18 @@ import '../models/task.dart';
 
 class TaskCard extends StatelessWidget {
   final Task task;
+
   const TaskCard({super.key, required this.task});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-      elevation: 3,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
       child: ListTile(
-        leading: CircleAvatar(
-          backgroundColor: Colors.blueAccent.withOpacity(0.2),
-          child: Text(
-            task.title.isNotEmpty ? task.title[0].toUpperCase() : "?",
-            style: const TextStyle(color: Colors.blue),
-          ),
-        ),
-        title: Text(
-          task.title,
-          style: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        subtitle: Text(
-          task.date ?? "Aucune date",
-          style: const TextStyle(color: Colors.grey),
-        ),
-        trailing: Icon(
-          task.isfavorite ? Icons.star : Icons.star_border,
-          color: task.isfavorite  ? Colors.amber: Colors.grey,
-          size: 28,
-        ),
-        onTap: () {
-          Navigator.pushNamed(context, "/task-detail",
-          arguments: task);
-        },
+        title: Text(task.title),
+        subtitle: Text(task.date ?? "Aucune date"),
       ),
     );
   }
 }
+
+
