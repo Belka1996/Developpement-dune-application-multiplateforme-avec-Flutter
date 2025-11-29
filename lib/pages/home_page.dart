0335d359fada67c5ecc3ccedaab6ce8e4fc2045c
massinissa mameri <massilockwood93@gmail.com>
2025-11-29 01:23:44 -0500

    update de home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,54 +1,116 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import '../models/task.dart';
 import '../widgets/task_card.dart';
 
-
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
-@override
+  @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Task> tasks =[
+  List<Task> tasks = [
     Task(id: "1", title: "Tache 1"),
-    Task(id: "1", title: "tache 2"),
-    Task(id: "3", title: "tache 3"),
+    Task(id: "2", title: "Tache 2"),
+    Task(id: "3", title: "Tache 3"),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Aujourd'hui"),
+      extendBodyBehindAppBar: true,
+
+     appBar: AppBar(
+   
+  centerTitle: true,
+  title: const Text(
+    "Aujourd'hui",
+    style: TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 20,
+    ),
+  ),
+  actions: [
+    IconButton(
+      icon: const Icon(Icons.search, color: Colors.red),
+      onPressed: () {
+        Navigator.pushNamed(context, "/search");
+      },
+    ),
+    IconButton(
+      icon: const Icon(Icons.logout, color: Colors.red),
+      onPressed: () {
+        Navigator.pushReplacementNamed(context, "/login");
+      },
+    ),
+  ],
+),
+
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+        
+          Image.asset(
+            "assets/images/home_bg.jpg",
+            fit: BoxFit.cover,
+          ),
+
+        
+          Container(
+            color: Colors.white.withOpacity(0.25),
+          ),
+
+
+          Padding(
+            padding: const EdgeInsets.only(top: 90),
+            child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () async {
+                    final result = await Navigator.pushNamed(
+                      context,
+                      '/task-detail',
+                      arguments: tasks[index],
+                    );
+
+                    if (result is Map) {
+                      final action = result["action"];
+                      final Task t = result["task"];
+
+                      setState(() {
+                        if (action == "edit") {
+                          tasks[index] = t;
+                        } else if (action == "delete") {
+                          tasks.removeWhere((e) => e.id == t.id);
+                        }
+                      });
+                    }
+                  },
+                  child: TaskCard(task: tasks[index]),
+                );
+              },
+            ),
+          ),
+        ],
       ),
-      body:ListView.builder(
-         itemCount: tasks.length,
-         itemBuilder: (context, index) {
-           return TaskCard(task: tasks[index]);
-         },
-      ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-         final result = await Navigator.pushNamed(context, '/new-task');
-         if (result is Map) {
-          final action = result["action"];
-          final Task updatedTask = result["task"];
-          setState(() {
-            if (action == "create") {
-                tasks.add(updatedTask);
-              } else if (action == "edit") {
-                final i = tasks.indexWhere((t) => t.id == updatedTask.id);
-                tasks[i] = updatedTask;
-              } else if (action == "delete"){
-                tasks.removeWhere((t)=> t.id == updatedTask.id);
-              }
+          final result = await Navigator.pushNamed(context, '/new-task');
+
+          if (result is Map && result["action"] == "create") {
+            setState(() {
+              tasks.add(result["task"]);
             });
-           }
-          },
-       child: const Icon(Icons.add),
+          }
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
 }
+
+
